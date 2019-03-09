3c43de8e7630d22258224ffda3591f116e08e6b9
romimitu <romimitu@gmail.com>
2019-03-10 02:20:01 +0600

    Updates

---
+++ precache-manifest.5625702fb6704e4d5e3a98ec01d5dffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "0cad40e0c76cd38ac980",
+    "url": "/contactmanager/static/js/main.0cad40e0.chunk.js"
+  },
+  {
+    "revision": "38fe078459cd3e83013f",
+    "url": "/contactmanager/static/js/2.38fe0784.chunk.js"
+  },
+  {
+    "revision": "0cad40e0c76cd38ac980",
+    "url": "/contactmanager/static/css/main.0aa38343.chunk.css"
+  },
+  {
+    "revision": "38fe078459cd3e83013f",
+    "url": "/contactmanager/static/css/2.b203f870.chunk.css"
+  },
+  {
+    "revision": "dea29efdb9710e672376bb06abde4970",
+    "url": "/contactmanager/index.html"
+  }
+];
